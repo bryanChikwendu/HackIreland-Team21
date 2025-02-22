1f4af32d504c954dbed540fdea788d2031eb2290
Bryan Chikwendu <26007246+bryanChikwendu@users.noreply.github.com>
2025-02-22 20:32:05 +0000

    JudgeBot Working Better

--- backend/judge_llm.py
+++ backend/judge_llm.py
@@ -2,7 +2,7 @@
 # Expecting to change this depending on how Eni wants it to be called
 from openai import OpenAI
 from flask import Flask, request
-
+import time
 with open(".env", 'r') as file:
     token = file.readlines()[1].strip('\n')
 
@@ -10,17 +10,19 @@ oclient = OpenAI(
     api_key=token
 )
 
+past_mes = [
+    {"role": "user",
+     "content": """You are an LLM designed to judge which action to take,
+       given text based updates on what is happening. You will receive timestamped updates
+       that are transcriptions of current events, then you must either output 'no action', or choose one from
+       'phone call to police', 'phone call to next-of-kin', 'notification to staff' """}
+]
 chat_completion = oclient.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "What is the fastest LLM on the market. Answer very briefly.",
-        }
-    ],
-    model="gpt-4o",
+    messages=past_mes,
+    model="gpt-4o-mini",
 )
-print(chat_completion)
-
+past_mes.append(chat_completion.choices[0].message)
+print(chat_completion.choices[0].message)
 app = Flask(__name__)
 
 @app.route("/")
@@ -29,6 +31,17 @@ def hello_world():
 
 @app.route("/update", methods=[ 'POST'])
 def upd():
+
     dat = request.data
-    print("Update:",  dat.decode())
-    return "Received"
+    bod = dat.decode()
+    t1 = time.time()
+    past_mes.append({"role":"user", "content":bod})
+    chat_completion = oclient.chat.completions.create(
+    messages=past_mes,
+    model="gpt-4o-mini",
+)
+    t2 = time.time()
+    resp = chat_completion.choices[0].message.content
+    resp += "; Took " + str(t2-t1) + "s"
+    past_mes.append(chat_completion.choices[0].message)
+    return resp
